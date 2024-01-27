c1d6cc72588411364ea5173142c7b47e62965c19
Jean Penso <jeanpaulpenso01@live.com>
2024-01-27 17:18:19 -0600

    modified appsettings.json

--- Project/Program.cs
+++ Project/Program.cs
@@ -5,9 +5,8 @@ using Project.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySQL(connectionString));
+    options.UseMySQL(Environment.GetEnvironmentVariable("ConnectionString")));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
