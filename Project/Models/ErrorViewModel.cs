70dcb0bd7bada42cc50d2e1c67664d0381e7f2ba
Jean Penso <jeanpaulpenso01@live.com>
2024-01-27 19:07:21 -0600

    added appsettings.json in gitignore

--- Project/Models/ErrorViewModel.cs
+++ Project/Models/ErrorViewModel.cs
@@ -5,5 +5,6 @@ namespace Project.Models
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
     }
 }
--- Project/Program.cs
+++ Project/Program.cs
@@ -4,9 +4,9 @@ using Project.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseMySQL(Environment.GetEnvironmentVariable("ConnectionString")));
+    options.UseMySQL(connectionString));
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
