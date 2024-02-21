4862377a048deac13b104f5dbc0c8e7092e805e0
Jean Penso <jeanpaulpenso01@live.com>
2024-02-21 14:42:05 -0600

    Added authorize attribute
    Make the user login first  before accessing the profile page

--- Project/Controllers/ProfileController.cs
+++ Project/Controllers/ProfileController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Project.Controllers
 {
+    // This will prompt the user to login first before accessing the Profile pages
+    [Authorize]
     public class ProfileController : Controller
     {
         public IActionResult Index()
