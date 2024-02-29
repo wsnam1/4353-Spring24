6c1b92f374cc2ba95db6a7ea8c558622994c44b0
Woosung Nam <>
2024-02-29 15:05:48 -0600

    error:syntax error

--- Project/Controllers/FuelQuoteController.cs
+++ Project/Controllers/FuelQuoteController.cs
@@ -47,21 +47,24 @@ namespace Project.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Index([Bind("GallonsRequested,DeliveryDate")] FuelHistory fuelQuote)
         {
-            // Check if the model state is valid.
-            if (ModelState.IsValid)
-            {
-                // This line doesn't insert the fuelQuote into the database yet; it just tracks the object in the EF context for further operations.
-                _context.Add(fuelQuote);
 
-                // Asynchronously save all changes made in the context to the database. This call actually inserts the fuelQuote into the database.
-                await _context.SaveChangesAsync();
+	        // Check if the model state is valid.
+	        if (ModelState.IsValid)
+	        {
+		        // This line doesn't insert the fuelQuote into the database yet; it just tracks the object in the EF context for further operations.
+		        _context.Add(fuelQuote);
 
-                // If the model state is valid and the data is saved successfully, redirect the user to the Index action of the Home controller.
-                
-            }
+		        // Asynchronously save all changes made in the context to the database. This call actually inserts the fuelQuote into the database.
+		        await _context.SaveChangesAsync();
 
-            // If the model state is not valid (e.g., required fields are missing or validation rules are not met), return the current view with the fuelQuote model. This allows the form to be re-displayed with the entered values and any validation messages.
-            return RedirectToAction("Index", "Home");
+		        // If the model state is valid and the data is saved successfully, redirect the user to the Index action of the Home controller.
+		        return RedirectToAction("Index", "Home");
+	        }
+
+	        // If the model state is not valid (e.g., required fields are missing or validation rules are not met), return the current view with the fuelQuote model. This allows the form to be re-displayed with the entered values and any validation messages.
+	        return View(fuelQuote);
+
+
         }
 
         // GET: /FuelQuote/History
