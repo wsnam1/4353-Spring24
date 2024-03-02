b3dbfda45cecb6da2040b6d609fd7faace9ff845
Jean Penso <jeanpaulpenso01@live.com>
2024-03-02 03:29:37 -0600

    Added non-nullable properties

--- Project/Models/UserProfile.cs
+++ Project/Models/UserProfile.cs
@@ -5,23 +5,23 @@ namespace Project.Models;
 public class UserProfile
 {
     [Key]
-    public string UserId { get; set; }
+    public string UserId { get; set; } = null!;
 
     [Required]
-    public string FullName { get; set; }
+    public string FullName { get; set; } = null!;
 
     [Required]
-    public string Address1 { get; set; }
+    public string Address1 { get; set; } = null!;
 
     public string? Address2 { get; set; }
 
     [Required]
-    public string City { get; set; }
+    public string City { get; set; } = null!;
 
     [Required]
-    public string State { get; set; }
+    public string State { get; set; } = null!;
 
     [Required]
     [StringLength(9, MinimumLength = 5)]
-    public string Zipcode { get; set; }
+    public string Zipcode { get; set; } = null!;
 }
