d9cd6c66f28931c5add8bd6639aaecf7ac88ebd8
Jean Penso <jeanpaulpenso01@live.com>
2024-03-02 10:58:42 -0600

    Added ViewBag for delivery address

--- Project/Controllers/FuelQuoteController.cs
+++ Project/Controllers/FuelQuoteController.cs
@@ -6,7 +6,6 @@ using Project.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 
-
 namespace Project.Controllers
 {
 	[Authorize]
@@ -24,6 +23,14 @@ namespace Project.Controllers
 		// GET: /FuelQuote
 		public IActionResult Index()
 		{
+            // Use a ViewBag the delivery address coming the from the user's profile to display it in the view 
+            var userId = _userManager.GetUserId(User);
+            var userProfile = _context.UserProfiles.SingleOrDefault(p => p.UserId == userId);
+
+			if (userProfile != null)
+			{
+                ViewBag.DeliveryAddress = userProfile.Address1;
+            }
 			
 			return View();
 		}
@@ -32,9 +39,9 @@ namespace Project.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Index(FuelHistory fuelQuote)
+        public async Task<IActionResult> Index([Bind("GallonsRequested,DeliveryAddress,DeliveryDate,SuggestedPrice,TotalAmountDue")] FuelHistory fuelQuote)
         {
-
+            // Get the Id of the current logged-in user
             var userId = _userManager.GetUserId(User);
 
 
@@ -55,10 +62,19 @@ namespace Project.Controllers
 
 		        // Asynchronously save all changes made in the context to the database. This call actually inserts the fuelQuote into the database.
 		        await _context.SaveChangesAsync();
+ 
 
-		        // If the model state is valid and the data is saved successfully, redirect the user to the Index action of the Home controller.
-		        return RedirectToAction("Index", "Home");
-  
+                // If the model state is valid and the data is saved successfully, redirect the user to the Index action of the Home controller.
+                return RedirectToAction("Index", "Home");
+            
+            }
+
+            // If saving data to db was unsuccessful we have to return the delivery address again to the view
+            var userProfile = _context.UserProfiles.SingleOrDefault(p => p.UserId == userId);
+
+            if (userProfile != null)
+            {
+                ViewBag.DeliveryAddress = userProfile.Address1;
             }
 
             // If the model state is not valid (e.g., required fields are missing or validation rules are not met), return the current view with the fuelQuote model. This allows the form to be re-displayed with the entered values and any validation messages.
@@ -68,13 +84,16 @@ namespace Project.Controllers
 
         // GET: /FuelQuote/History
         public async Task<IActionResult> History()
-		{
+        {
 
-            var userId = _userManager.GetUserId(User);
+            // Step 1: Get the id of the user
 
-			
-            // Asynchronously return all the rows in the FuelHistories table to the view.
-            return View(await _context.FuelHistories.ToListAsync());
+            // Step 2: Get a list of all the fuel quote histories from the FuelHistories table in the db that match the user id
+            // You can use the .Where() and .ToListAsync() methods for this
+
+            // Step 3: Pass the list of histories to the view using the return statement
+
+            return View();
 		}
 	}
 }
