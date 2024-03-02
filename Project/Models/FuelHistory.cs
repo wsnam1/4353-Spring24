5c6f6ce5077b1072ac1f8ef754b023c6bf31b30f
Jean Penso <jeanpaulpenso01@live.com>
2024-03-02 03:29:15 -0600

    Updated properties of class for new table

--- Project/Models/FuelHistory.cs
+++ Project/Models/FuelHistory.cs
@@ -1,15 +1,23 @@
-using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
 
 namespace Project.Models;
 
 
 public class FuelHistory
 {
-    public int Id { get; set; }
-
+    [Key]
     [Required]
+    public string UserId { get; set; } = null!;
+
     public int GallonsRequested { get; set; }
-    
-    public string DeliveryDate { get; set; }
-    
+
+    public string DeliveryAddress { get; set; } =  null!;
+
+    public string DeliveryDate { get; set; } = null!;
+
+    public decimal SuggestedPrice { get; set; }
+
+    public decimal TotalAmountDue { get; set; }
+
 }
